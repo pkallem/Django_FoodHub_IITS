63cf5e2fe360fc33092db5ce48a0a065b2e9c5e1
pkallem <pranav.kallem@gmail.com>
2021-07-19 15:47:27 -0400

    admin locked pages

--- foodhub/views.py
+++ foodhub/views.py
@@ -1,3 +1,4 @@
+from foodhub.decorators import unauthenticated_user
 from django import forms
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
@@ -6,8 +7,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import CreateUserForm
 from django.contrib.auth.decorators import login_required
+from .decorators import allowed_users, unauthenticated_user
 
 @login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
 def homePage(request):
     return render(request,'index.html')
 
@@ -28,21 +31,20 @@ def registerPage(request):
     context = {'form':form}
     return render(request, 'register.html', context)
 
+@unauthenticated_user
 def loginPage(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
 
-            user = authenticate(request, username=username, password=password)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                messages.info(request, 'Username or password is incorrect.')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or password is incorrect.')
 
     context = {}
     return render(request, 'login.html', context)
